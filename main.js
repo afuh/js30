604c152b839f3f5c55a0b2e004cbb76be9ea6a53
Axel <axelfuh@gmail.com>
2017-04-12 19:57:42 +0200

    Update showcase JS

--- main.js
+++ main.js
@@ -1,3 +1,23 @@
+function addContent(){
+  const showcase = document.getElementById('showcase');
+
+  return data.map(data => {
+    const render =
+    `<div class='box'>
+      <a target='_blank' href='${data.url}'>
+        <img alt='JS30' src='${data.img}'>
+        <div class='title-grad'>
+          <span class='title'>${data.title}</span>
+        </div>
+      </a>
+    </div>`;
+
+    showcase.insertAdjacentHTML("beforeend", render);
+    });
+}
+
+window.onload = addContent;
+
 const data = [
   {
     title: "JavaScript Drum Kit",
@@ -15,42 +35,3 @@ const data = [
     img: "03 - CSS Variables/image/03_port.jpg"
   }
 ];
-
-function addContent(){
-  const showcase = document.getElementById('showcase');
-
-  return data.map((data, i) => {
-    let render = "";
-    render += "<div class='box'>";
-    render += `<a target='_blank' href='${data.url}'>`;
-    render += `<img alt='JS30' src='${data.img}'>`;
-    render += "<div class='title-grad'>";
-    render += `<span class='title'>${data.title}</span>`;
-    render += "</div>";
-    render += "</a>";
-    render += "</div>";
-
-    showcase.insertAdjacentHTML("beforeend", render);
-
-    // const box = document.createElement("div");
-    // const link = document.createElement("a");
-    // const img = document.createElement("img");
-    // const span = document.createElement("span");
-    //
-    // box.appendChild(link);
-    // link.appendChild(img);
-    // link.appendChild(span);
-    // showcase.appendChild(box);
-    //
-    // box.className = "box";
-    // link.target = "_blank";
-    // img.alt = "JS30";
-    // span.className = "title";
-    //
-    // span.innerHTML = data.title;
-    // link.href = data.url;
-    // img.src = data.img;
-    });
-}
-
-window.onload = addContent;
