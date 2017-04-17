e55bd9af5af9cfb5fd736a154a9101a4c42b32d5
Axel <axelfuh@gmail.com>
2017-04-17 15:31:56 +0200

    Update exercise 10 style

--- 10 - Hold Shift and Check Checkboxes/main.js
+++ 10 - Hold Shift and Check Checkboxes/main.js
@@ -1,3 +1,27 @@
+const chance = new Chance();
+
+(function(){
+  const mails = 12;
+  const inbox = document.getElementById('inbox');
+
+  for (let i = 0; i < mails; i++) {
+    const render =
+    `
+    <div class="item">
+      <input type="checkbox">
+      <div class="mail">
+        <p class="name">${chance.name()}</p>
+        <p class="title">${chance.sentence({words: 3})}</p>
+        <p class="intro">${chance.sentence({words: 16})}</P>
+        <p class="date">${chance.date({string: true, american: false, year: 2017})}</P>
+      </div>
+    </div>
+    `;
+
+    inbox.insertAdjacentHTML("beforeend", render);
+  }
+})();
+
 const checkboxes = document.querySelectorAll("input[type='checkbox']");
 let lastChecked;
 
